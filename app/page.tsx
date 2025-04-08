844a9163459b803f8df31cb0961370f9c3527df9
skadell <trymskadelltsa@gmail.com>
2025-04-08 15:29:42 +0200

    musikkmappe page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,5 +15,11 @@ export default function Home() {
         </p>
       </div>
     </main>
+export default function Page() {
+  return (
+    <main className="min-h-screen bg-[#0033A0] text-white p-10">
+      <h1 className="text-4xl font-bold mb-6">Musikk</h1>
+      <p className="text-lg">Her kommer alt jeg vil dele innen musikk. Remixer, anbefalinger</p>
+    </main>
   );
 }
