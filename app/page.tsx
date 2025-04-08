e249e8e2b4f097ecf6d9261f06b92f375aa2ea58
skadell <trymskadelltsa@gmail.com>
2025-04-08 18:05:44 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,6 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#0033A0] text-white p-10 flex flex-col md:flex-row items-center justify-center gap-10">
-       </p><Link href="/Musikk">
-  <button className="mt-6 px-6 py-3 bg-white text-[#0033A0] font-semibold rounded-full hover:bg-gray-200 transition">
-    Musikk
-  </button>
-</Link>
-        <Link href="/linser">
-  <button className="mt-4 px-6 py-3 bg-white text-[#0033A0] font-semibold rounded-full hover:bg-gray-200 transition">
-    Linser
-  </button>
-</Link>
-        <Link href="/tekst">
-  <button className="mt-4 px-6 py-3 bg-white text-[#0033A0] font-semibold rounded-full hover:bg-gray-200 transition">
-    Tekst
-  </button>
-</Link>      <img
         src="/fanzine-v-2.png"
         alt="Trym"
         className="w-64 rounded-lg shadow-lg"
