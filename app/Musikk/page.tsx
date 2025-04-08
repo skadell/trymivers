8d9c93a811e107e821c64d831011eaaec1d76a4c
skadell <trymskadelltsa@gmail.com>
2025-04-08 17:11:53 +0200

    Update page.tsx

--- app/Musikk/page.tsx
+++ app/Musikk/page.tsx
@@ -2,6 +2,9 @@ import Link from 'next/link';
 export default function MusikkPage() {
   return (
     <main className="min-h-screen bg-[#0033A0] text-white p-10">
+       <Link href="/">
+        <button className="text-white text-xl mb-6 hover:underline">← Tilbake</button>
+      </Link>     
       <h1 className="text-4xl font-bold mb-6">Musikk</h1>
       <p className="text-lg leading-relaxed">
         Her kommer alt jeg vil dele innen musikk. Remixer og anbefalinger.
