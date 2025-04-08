0669f8fa95febb511a682c058a35e500ec5b5576
skadell <trymskadelltsa@gmail.com>
2025-04-08 15:06:46 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,3 @@
-'use client';
-import { useEffect, useState } from 'react';
-export default function Home() {
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
-
-useEffect(() => {
-  const handleMouseMove = (e: MouseEvent) => {
-    setMousePos({ x: e.clientX, y: e.clientY });
-  };
-  window.addEventListener('mousemove', handleMouseMove);
-  return () => window.removeEventListener('mousemove', handleMouseMove);
-}, []);
 return (
   <img
   src="/trymivers_helkropp_transparent.png"
