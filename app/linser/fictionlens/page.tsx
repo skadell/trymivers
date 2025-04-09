ae8e37ecd7a94543815dc0a0d36caaf514241450
skadell <trymskadelltsa@gmail.com>
2025-04-09 17:06:56 +0200

    Update page.tsx

--- app/linser/fictionlens/page.tsx
+++ app/linser/fictionlens/page.tsx
@@ -57,7 +57,7 @@ export default function Fictionlens() {
           ref={canvasRef}
           width={420}
           height={300}
-          className="absolute top-[275px] left-[90px]"
+          className="absolute top-[275px] left-[120px]"
         />
         <video ref={videoRef} autoPlay playsInline className="hidden" />
         <img
