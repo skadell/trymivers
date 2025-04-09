c112142d9b2f0527946aba45de77b8863bec68a0
skadell <trymskadelltsa@gmail.com>
2025-04-09 23:21:32 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -32,8 +32,7 @@ export default function Home() {
         <img
           src="/fanzine-v-2.png"
           alt="Trym"
-          className="w-64 rounded-lg shadow-lg"
-        />
+          className="w-64 rounded-lg shadow-lg relative -top-10 -left-10"        />
         <div>
           <h1 className="text-4xl font-bold mb-4">Velkommen til Trymivers</h1>
           <p className="text-lg max-w-md">
